5e158c17b585203b472a4fe5e25b089bc5ada43b
Daniel Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2023-06-24 16:26:32 -0500

    - created homescreen - added sportsnavigator menu component

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import Homescreen from './screens/Homescreen'
 import { API_URL } from '@env'
 
 export default function App() {
   return (
     <NavigationContainer>
       <SafeAreaView>
-        <Text className='text-red-400'>
-          Open up App.js to start working on your app! {API_URL}
-        </Text>
-        <StatusBar style='auto' />
+        <Homescreen />
       </SafeAreaView>
     </NavigationContainer>
   )
