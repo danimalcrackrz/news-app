6f1c05c21bd7b1d4e068490731cf3e4919ac0c5a
Daniel Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2023-06-24 15:45:41 -0500

    - created .env link

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { API_URL } from '@env'
 
 export default function App() {
   return (
     <NavigationContainer>
       <SafeAreaView>
         <Text className='text-red-400'>
-          Open up App.js to start working on your app!
+          Open up App.js to start working on your app! {API_URL}
         </Text>
         <StatusBar style='auto' />
       </SafeAreaView>
