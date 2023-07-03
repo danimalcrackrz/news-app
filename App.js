8bfea6c7d1949e150f26f20b2b6dd965926d3d72
Daniel Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2023-07-03 14:48:41 -0500

    - fixed import

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { RecoilRoot } from 'recoil'
-import NewsScreen from './screens/NewsScreen'
+import Newsscreen from './screens/Newsscreen'
 
 export default function App() {
   return (
     <NavigationContainer>
       <RecoilRoot>
         <SafeAreaView className='bg-zinc-900 flex-1'>
-          <NewsScreen />
+          <Newsscreen />
         </SafeAreaView>
       </RecoilRoot>
     </NavigationContainer>
