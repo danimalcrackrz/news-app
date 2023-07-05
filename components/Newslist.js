dd67d3478864b8295615719f6096f23b6b74d5c7
Daniel Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2023-07-04 19:49:59 -0500

    updated font color in newslist

--- components/Newslist.js
+++ components/Newslist.js
@@ -7,7 +7,7 @@ const Newslist = () => {
   const articleList = useRecoilValue(NEWSSTATE)
   return (
     <View className='p-4 flex-shrink'>
-      <Text className='text-white text-4xl font-bold capitalize mb-4'>
+      <Text className='text-red-600 text-4xl font-bold capitalize mb-4'>
         top stories
       </Text>
       <FlatList
